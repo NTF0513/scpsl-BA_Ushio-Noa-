611072f6f782e77f532221e432a75351f2baf7d2
NTF0513 <LiWentao191919666@163.com>
2025-05-17 10:09:22 +0800

    Add files via upload
    此插件在回合开始时在保安中随机选取一名玩家 血量125 比普通保安多一支肾上腺素和一把维克托 死亡有cassie广播

---
+++ Config.cs
@@ -0,0 +1,33 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace Ushio_Noa
+{
+    public class Config : IConfig
+    {
+        [Description("是否开启诺亚插件")]
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; }
+    }
+    public class Plugin : Plugin<Config>
+    {
+        private EventHandlers EventHandlers;
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Log.Info("Noa(诺亚)插件已启动 by九尾狐0513");
+            EventHandlers = new EventHandlers();
+            Exiled.Events.Handlers.Server.RoundStarted += EventHandlers.RoundStarted;
+            Exiled.Events.Handlers.Player.Died += EventHandlers.PlayerDied;
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Exiled.Events.Handlers.Server.RoundStarted -= EventHandlers.RoundStarted;
+            Exiled.Events.Handlers.Player.Died -= EventHandlers.PlayerDied;
+            EventHandlers = null;
+        }
+    }
+
+}
---
+++ EventHandler.cs
@@ -0,0 +1,42 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using System.Collections.Generic;
+
+namespace Ushio_Noa
+{
+    public class EventHandlers
+    {
+        public static void RoundStarted()
+        {
+            foreach (Player player in Player.List)
+            {
+                if (Noa.Count < 1 && player.Role.Type == PlayerRoles.RoleTypeId.FacilityGuard)
+                {
+                    _Noa(player);
+                }
+            }
+        }
+        public static void _Noa(Player player)
+        {
+            Noa.Add(player);
+            player.Health = 125f;
+            player.MaxHealth = 125f;
+            player.RankColor = "cyan";
+            player.RankName = "生盐 诺亚";
+            player.AddItem((ItemType)21);
+            player.AddItem((ItemType)33);
+            player.ShowHint("<color=#87CEEB>你是\n <color=#FFC0CB>生盐诺亚</color>\n 帮助警卫维持设施秩序 同时寻找逃出的小雪</color>", 5f);
+        }
+        public void PlayerDied(DiedEventArgs hub)
+        {
+            if (Noa.Contains(hub.Player))
+            {
+                Cassie.MessageTranslated("N O A is died", "<color=#87CEEB>诺亚:返回疗伤</color>");
+                Noa.Remove(hub.Player);
+                hub.Player.RankColor = null;
+                hub.Player.RankName = null;
+            }
+        }
+        public static List<Player> Noa = new List<Player>();
+    }
+}
